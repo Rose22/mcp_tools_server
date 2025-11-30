2d4b21c0421de40cd2dfa2f49d17220c1fe629be
Rose22 <rosemelody22@gmail.com>
2025-11-30 23:35:22 +0100

    renamed websearch module

--- mcp_tools/__init__.py
+++ mcp_tools/__init__.py
@@ -1,4 +1,4 @@
-from mcp_tools import system, files, reader, networking, markdown_db, search
+from mcp_tools import system, files, reader, networking, markdown_db, websearch
 
 def register_mcp(mcp):
     """
@@ -9,4 +9,4 @@ def register_mcp(mcp):
     reader.register_mcp(mcp)
     networking.register_mcp(mcp)
     markdown_db.register_mcp(mcp)
-    search.register_mcp(mcp)
+    websearch.register_mcp(mcp)
