72bf7cc296e3ab08e9b41596129fd2df17b2b005
Rose22 <rosemelody22@gmail.com>
2026-01-30 01:42:32 +0100

    forgot to add mcp registerer

--- mcp_tools/time.py
+++ mcp_tools/time.py
@@ -2,15 +2,18 @@ import utils
 import datetime
 
 def register_mcp(mcp):
+    @mcp.tool
     def get_datetime() -> dict:
         """gets the current time and date. ALWAYS call this before assuming what date today is! NEVER assume or guess today's date."""
 
     return utils.result(datetime.datetime.now().strftime("%c"))
 
+    @mcp.tool
     def get_time() -> dict:
         """gets the current time"""
         return utils.result(datetime.datetime.now().strftime("%X"))
 
+    @mcp.tool
     def get_date() -> dict:
         """gets the current date (without time)"""
         return utils.result(datetime.datetime.now().strftime("%x"))
